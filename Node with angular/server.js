d525c1635ba57c67a68187197e7f208485c049ac
PaneerSelvam93 <panner1993a@gmail.com>
2017-03-30 20:31:24 +0530

    Add files via upload

---
+++ Node with angular/server.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+app = express();
+app.get('/',function(req,res){
+	res.sendfile(__dirname + '/client/views/index.html');
+});
+
+app.use('/js',express.static(__dirname+'/client/js'));
+
+app.listen(3000,function(){
+	console.log('Im listening...');
+})
